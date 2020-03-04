b58b503b8699e6aa1335887bd0987b73e79738ef
PetrLak <60690481+PetrLak@users.noreply.github.com>
2020-03-04 11:38:17 +0200

    Add function fo addtition.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Repository to store files related to CMiM course at LUT in 2020
 
 First commit.
+
+Add function that adds numbers.
---
+++ add_test.m
@@ -0,0 +1,6 @@
+function r = add_test(a,b)
+%ADD_TEST Summary of this function goes here
+%   Detailed explanation goes here
+r = a + b;
+end
+
